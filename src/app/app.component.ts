ffe739673d7cdbaa9c45ef1af25a4e514f5ac51c
Muhammet Şükrü Kalpaklıoğlu <77413367+mskalpaklioglu@users.noreply.github.com>
2023-08-21 02:37:41 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {lorem} from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'harf';
+
+  randomText = lorem.sentence();
+  enteredText = '';
+
+
+  getInputValue(value: string)
+  {
+    console.log(this.randomText.split(''));
+    this.enteredText=value;
+
+  }
+
+  compare(randomLetter: string , enteredLetter: string)
+  {
+    if(!enteredLetter)
+    {
+      return 'pending';
+    }
+
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
